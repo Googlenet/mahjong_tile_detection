a7c50f0e2fbe33d3d065dd3e59abbf8391656426
Jonah Hu <jonah.hu123@gmail.com>
2023-03-01 22:56:31 -0800

    Initial push (2/23/23 start)

---
+++ TinyVGGaug.py
@@ -0,0 +1,399 @@
+from typing import List
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from torch import nn
+import torch
+from torchinfo import summary
+from tqdm.auto import tqdm
+import functions
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
+import numpy as np
+from pathlib import Path
+
+# Create simple transform
+simple_transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor()
+])
+
+trivial_transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.TrivialAugmentWide(num_magnitude_bins=31),
+    transforms.ToTensor()
+])
+
+rot_transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.RandomRotation(degrees=30),
+    transforms.ToTensor()
+])
+
+gray_transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.Grayscale(3),
+    transforms.ToTensor()
+])
+
+# 1. Load and transform data
+train_dir = 'tiles/train'
+test_dir = 'tiles/test'
+
+train_data_aug = datasets.ImageFolder(root=train_dir, transform=trivial_transform)
+train_data_simple = datasets.ImageFolder(root=train_dir, transform=simple_transform)
+train_data_gray = datasets.ImageFolder(root=train_dir, transform=gray_transform)
+train_data_rot = datasets.ImageFolder(root=train_dir, transform=rot_transform)
+
+#train_data_crop = datasets.ImageFolder(root=train_dir, transform=crop_transform)
+
+train_data = torch.utils.data.ConcatDataset([train_data_aug, train_data_simple,
+                                             train_data_gray, train_data_rot])
+
+
+test_data_simple = datasets.ImageFolder(root=test_dir, transform=simple_transform)
+
+# 2. Turn data into DataLoaders
+# Setup batch size and number of workers
+BATCH_SIZE = 32
+print(f"Creating DataLoader's with batch size {BATCH_SIZE}.")
+
+# Create DataLoader's
+train_dataloader_aug = DataLoader(train_data,
+                                     batch_size=BATCH_SIZE,
+                                     shuffle=True)
+
+test_dataloader_simple = DataLoader(test_data_simple,
+                                    batch_size=BATCH_SIZE,
+                                    shuffle=False)
+
+# print(train_dataloader_simple, test_dataloader_simple)
+"""
+class TinyVGG(nn.Module):
+    
+    # Model architecture copying TinyVGG from:
+    # https://poloclub.github.io/cnn-explainer/
+    
+
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,  # how big is the square that's going over the image?
+                      stride=1,  # default
+                      padding=0),
+            # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)  # default stride value is same as kernel_size
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # Where did this in_features shape come from?
+            # It's because each layer of our network compresses and changes the shape of our inputs data.
+            nn.Linear(in_features=hidden_units * 29 * 29,
+                      out_features=output_shape)
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        # print(x.shape)
+        x = self.conv_block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
+        return x
+        # return self.classifier(self.conv_block_2(self.conv_block_1(x))) # <- leverage the benefits of operator fusion
+"""
+class Custom(nn.Module):
+    """
+    Model architecture copying TinyVGG from:
+    https://poloclub.github.io/cnn-explainer/
+    """
+
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,  # how big is the square that's going over the image?
+                      stride=1,  # default
+                      padding=0),
+            # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number
+            nn.ReLU(),
+            # nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=0),
+            # nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)  # default stride value is same as kernel_size
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            # nn.Conv2d(hidden_units, hidden_units, kernel_size=3, stride=1, padding=0),
+            # nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # Where did this in_features shape come from?
+            # It's because each layer of our network compresses and changes the shape of our inputs data.
+            nn.Linear(in_features=hidden_units * 30 * 30,
+                      out_features=output_shape)
+        )
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv_block_1(x)
+        # print(x.shape)
+        x = self.conv_block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
+        return x
+        # return self.classifier(self.conv_block_2(self.conv_block_1(x))) # <- leverage the benefits of operator fusion
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#torch.manual_seed(42)
+model_0 = Custom(input_shape=3,  # number of color channels (3 for RGB)
+                  hidden_units=3,
+                  output_shape=len(test_data_simple.classes)).to(device)
+
+print(len(test_data_simple.classes))
+print(model_0)
+
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer):
+    # Put model in train mode
+    model.train()
+
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+
+    # Loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate  and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Calculate and accumulate accuracy metric across all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item() / len(y_pred)
+
+    # Adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+def test_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module):
+    # Put model in eval mode
+    model.eval()
+
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            test_pred_logits = model(X)
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            # Calculate and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += (test_pred_labels == y).sum().item() / len(test_pred_labels)
+
+    # Adjust metrics to get average loss and accuracy per batch
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+# 1. Take in various parameters required for training and test steps
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5):
+    # 2. Create empty results dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+               }
+
+    # 3. Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer)
+        test_loss, test_acc = test_step(model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn)
+
+        # 4. Print out what's happening
+        print(
+            f"Epoch: {epoch + 1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        # 5. Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    # 6. Return the filled results at the end of the epochs
+    return results
+
+
+# Set random seeds
+#torch.manual_seed(42)
+#torch.cuda.manual_seed(42)
+
+# Set number of epochs
+NUM_EPOCHS = 10
+
+# Setup loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model_0.parameters(), lr=0.001)
+
+# Start the timer
+from timeit import default_timer as timer
+start_time = timer()
+
+# Train model_0
+model_0_results = train(model=model_0,
+                        train_dataloader=train_dataloader_aug,
+                        test_dataloader=test_dataloader_simple,
+                        optimizer=optimizer,
+                        loss_fn=loss_fn,
+                        epochs=NUM_EPOCHS)
+
+# End the timer and print out how long it took
+end_time = timer()
+print(f"Total training time: {end_time-start_time:.3f} seconds")
+
+# plotting loss curves
+functions.plot_loss_curves(model_0_results).show()
+
+# Create model save path ------------------------------------------------
+MODEL_PATH = Path("models")
+MODEL_NAME = "ver7_model.pth"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+# Save the model state dict
+print(f"Saving model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model_0.state_dict(), # only saving the state_dict() only saves the learned parameters
+           f=MODEL_SAVE_PATH)
+
+
+
+def pred_and_plot_image(model: torch.nn.Module,
+                        image_path: str,
+                        class_names: List[str] = None,
+                        transform = None,
+                        device: torch.device = device):
+    """Makes a prediction on a target image and plots the image with its prediction."""
+
+    # 1. Load in image and convert the tensor values to float32
+    target_image = torchvision.io.read_image(str(image_path)).type(torch.float32)
+
+    # 2. Divide the image pixel values by 255 to get them between [0, 1]
+    target_image = target_image / 255.
+
+    # 3. Transform if necessary
+    if transform:
+        target_image = transform(target_image)
+
+    # 4. Make sure the model is on the target device
+    model.to(device)
+
+    # 5. Turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # Add an extra dimension to the image
+        target_image = target_image.unsqueeze(dim=0)
+
+        # Make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(target_image.to(device))
+
+    # 6. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # 7. Convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+    # 8. Plot the image alongside the prediction and prediction probability
+    plt.imshow(target_image.squeeze().permute(1, 2, 0))  # make sure it's the right size for matplotlib
+    if class_names:
+        title = f"Pred: {class_names[target_image_pred_label.cpu()]} | Prob: {target_image_pred_probs.max().cpu():.3f}"
+    else:
+        title = f"Pred: {target_image_pred_label} | Prob: {target_image_pred_probs.max().cpu():.3f}"
+    plt.title(title)
+    plt.axis(False)
+    plt.show()
+
+custom_image_path = 'tiles/test/flower_3/IMG_20230226_194738153.jpg'
+class_names = test_data_simple.classes
+custom_image_transform = transforms.Compose([
+    transforms.Resize((128, 128))
+])
+device = 'cpu'
+
+# Pred on our custom image
+pred_and_plot_image(model=model_0,
+                    image_path=custom_image_path,
+                    class_names=class_names,
+                    transform=custom_image_transform,
+                    device=device)
+
+
+
+
